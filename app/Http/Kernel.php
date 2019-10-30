6500d0644aed815c02fed67e85a24f9c0846d62e
ajengpras <salzabillaajeng12@gmail.com>
2019-10-30 14:28:55 +0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'UserLevelMiddleware' => \App\Http\Middleware\userLevelMiddleware::class,
+        'UserLevelMiddleware' => \App\Http\Middleware\UserLevelMiddleware::class,
     ];
 
     /**
